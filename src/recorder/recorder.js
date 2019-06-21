39ee833586dc62d658699fa2dbc3898a48dc80f0
Ivo Parun Rua <ivo.parun.rua@testrigor.com>
2019-06-20 21:11:03 -0300

    Sended the value of inputs, #166834202

--- src/recorder/recorder.js
+++ src/recorder/recorder.js
@@ -8,16 +8,19 @@ export default class Recorder {
   constructor(options) {
     this.eventListener = new EventListener();
     this.token = options.token;
-    // eslint-disable-next-line no-undef
-    this.webSocket = webSocket(`${RECORDER_URL}/events?API_TOKEN=${this.token}&clientId=${Session.getSession() || ''}`);
-    this.webSocket.pipe(
-      retryWhen(errors =>
-        errors.pipe(
-          delay(5000)
+    if (Recorder.shouldConnect()) {
+      // eslint-disable-next-line no-undef,max-len
+      this.webSocket = webSocket(`${RECORDER_URL}/events?API_TOKEN=${this.token}&clientId=${Session.getSession() || ''}`);
+      this.webSocket.pipe(
+        retryWhen(errors =>
+          errors.pipe(
+            delay(5000)
+          )
         )
-      )
-    ).subscribe();
-    this.subscribeToEvents();
+      ).subscribe();
+      this.subscribeToEvents();
+    }
+
   }
 
   subscribeToEvents() {
@@ -26,5 +29,20 @@ export default class Recorder {
       .subscribe((event) => {
         this.webSocket.next(event);
       });
+  }
+
+  static shouldConnect() {
+    let cookies = document.cookie.split(';');
+
+    let length = cookies.length;
+
+    for (let i = 0; i < length; i++) {
+      let cookie = cookies[i].split('=');
+
+      if (cookie.length && cookie[0].trim() === '44523a90-a964-49b9-84e5-b04fd8981730') {
+        return false;
+      }
+    }
+    return true;
   }
 }
